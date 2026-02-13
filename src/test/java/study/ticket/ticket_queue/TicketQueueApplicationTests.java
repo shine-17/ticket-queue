fe1df45cdea23e223db8b187324b056b3fd61a13
dohun <shine_17@naver.com>
2026-02-13 22:49:56 +0900

    init

---
+++ src/test/java/study/ticket/ticket_queue/TicketQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ticket.ticket_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
