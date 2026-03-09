9a9bd467359a1a2606b25061085ed40c4f96b327
dohun <shine_17@naver.com>
2026-03-09 23:34:10 +0900

    feat: modify issue token process

---
+++ src/main/java/study/ticket/ticket_queue/redis/queue/RedisWaitingQueue.java
@@ -0,0 +1,111 @@
+package study.ticket.ticket_queue.redis.queue;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.stereotype.Component;
+import study.ticket.ticket_queue.port.WaitingQueuePort;
+import study.ticket.ticket_queue.redis.util.RedisKeys;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RedisWaitingQueue implements WaitingQueuePort {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Value("${ticket.booking.capacity}")
+    private int CAPACITY;
+
+
+    @Override
+    public boolean isActive(String userId, long showId) {
+        String key = RedisKeys.ACTIVE_USER.generateKey(showId);
+        Long result = redisTemplate.opsForZSet().rank(key, userId);
+
+        return result != null;
+    }
+
+    @Override
+    public boolean isWaiting(String userId, long showId) {
+        String key = RedisKeys.WAITING_USER.generateKey(showId);
+        Long result = redisTemplate.opsForZSet().rank(key, userId);
+
+        return result != null;
+    }
+
+    @Override
+    public void add(String userId, long showId) {
+        long time = Timestamp.valueOf(LocalDateTime.now()).getTime();
+        String key = RedisKeys.WAITING_USER.generateKey(showId);
+
+        Boolean result = redisTemplate.opsForZSet().add(key, userId, time);
+
+        if (result == null || !result) {
+            throw new IllegalStateException("Redis operations failed to add user \"" + userId + "\" to waiting queue");
+        }
+    }
+
+    @Override
+    public long findRank(String loginId, long showId) {
+        // 인덱스 0부터 시작
+        return redisTemplate.opsForZSet().rank(RedisKeys.WAITING_USER.generateKey(showId), loginId) + 1;
+    }
+
+    private void issueToken(String userId, long showId) {
+        // Lua Script
+        RedisScript<String> script = getLuaScript("script/redis/issue_token.lua", String.class);
+
+        // key[1]: active key, key[2]: waiting key
+        List<String> keys = getKeys(showId);
+
+        // ARGV: userId, seatCount, ttl
+        String result = redisTemplate.execute(
+                script,
+                keys,
+                userId,
+                CAPACITY,
+                Timestamp.valueOf(LocalDateTime.now()).getTime()
+        );
+
+
+        if (result == null) {
+            throw new IllegalStateException("");
+        }
+        else if (result.equals("ALREADY_ACTIVE")) {
+            throw new IllegalStateException("이미 예약된 좌석입니다.");
+        }
+        else if (result.equals("ALREADY_WAITING")) {
+            throw new IllegalStateException("이미 선점된 좌석입니다.");
+        }
+        else if (result.equals("ADD_ACTIVE")) {
+
+        }
+        else if (result.equals("ADD_WAITING")) {
+
+        }
+
+        // result == 1이면 성공
+    }
+
+    private <T> RedisScript<T> getLuaScript(String resourcePath, Class<T> resultType) {
+        DefaultRedisScript<T> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource(resourcePath));
+        script.setResultType(resultType);
+        return script;
+    }
+
+    private List<String> getKeys(long showId) {
+        // active key, waiting key
+        return List.of(
+                RedisKeys.ACTIVE_USER.generateKey(showId),
+                RedisKeys.WAITING_USER.generateKey(showId)
+        );
+    }
+}
